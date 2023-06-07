d1a297d2d01a1b52984b3379543b5b080c9a5eb8
Fredy Santamaria <fsantamaria@berryit.net>
2023-06-07 09:59:05 -0400

    refactor: Add more widgets
    -Added a file uploader
    -Added a button
    -Added a spinner

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import streamlit as st
-st.write("")
+import time
+
+st.title("Bird Classifier")
+st.header(":bird: :turkey: :peacock: :swan:  :owl: :duck: :flamingo: :eagle: :parrot:")
+st.markdown('_By Fredy Santamaria_')
+
+bird_image = st.file_uploader("Please upload an image of a bird", type="jpg")
+
+classify_button = st.button("Get Bird Species")
+if classify_button:
+    if bird_image:
+        with st.spinner(text='In progress...'):
+            time.sleep(5)
+            st.success('Done')
+    else:
+        st.error('Please upload an image')
