8afc808ca563c742d06cdeb2d47a3439bf42fe4b
Fredy Santamaria <fsantamaria@berryit.net>
2023-06-08 11:59:42 -0400

    refactor: Modify the widgets
    -Add a subheader add at the bottom with the predicted class
    -Remove the bar graph

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import time
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 
@@ -21,39 +19,28 @@ bird_image = st.file_uploader("Please upload an image of a bird", type="jpg")
 classify_button = st.button("Get Bird Species")
 if classify_button:
     if bird_image:
-        img = image.load_img(bird_image, target_size=(224, 224))
-        img_array = image.img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array /= 255.0
 
         with st.spinner(text='In progress...'):
+            # Preprocess image
+            img = image.load_img(bird_image, target_size=(224, 224))
+            img_array = image.img_to_array(img)
+            img_array = np.expand_dims(img_array, axis=0)
+            img_array /= 255.0
+
             # Perform the prediction
             predictions = model.predict(img_array)[0]
             st.success('Done')
 
-        # Get the top 5 predicted classes
-        top_5_indices = np.argsort(predictions)[-5:][::-1]
-        top_5_class_labels = []
-        top_5_probabilities = []
-        for i in top_5_indices:
-            class_id = i
-            idx = birds_df.index[birds_df['class id'] == class_id].tolist()
+            # Get the index of the highest predicted class
+            top_index = np.argmax(predictions)
+            class_id = top_index - 1
+            idx = birds_df[birds_df['class id'] == class_id].index.tolist()
             if idx:
                 label = birds_df.loc[idx[0], 'labels']
-                probability = predictions[i] * 100
-                top_5_class_labels.append(label)
-                top_5_probabilities.append(probability)
+                probability = predictions[top_index] * 100
 
-        # Create a bar graph
-        fig, ax = plt.subplots()
-        ax.bar(top_5_class_labels, top_5_probabilities)
-        ax.set_xlabel('Species')
-        ax.set_ylabel('Probability (%)')
-        ax.set_title('Top 5 Predicted Bird Species')
-        ax.tick_params(axis='x', rotation=45)
-        plt.tight_layout()
-
-        st.pyplot(fig)
+        # Display the predicted class name
+        st.subheader(f"Predicted Bird Species: {label}")
 
     else:
         st.error('Please upload an image')
